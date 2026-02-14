13143ff5058a37537ccbfc10ab6f036bf73d9d92
caporal404 <kevincode26@gmail.com>
2026-02-14 23:14:48 +0100

    feat(ui): add theme button

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import TaskProvider from './providers/taskProvider';
 import TaskForm from './components/TaskForm/TaskForm';
 import TaskList from './components/TaskList/TaskList';
 import FilterControls from './components/FilterControls/FilterControls';
+import iconMoon from '/assets/images/icon-moon.svg'
 import './App.css'
 
 function App() {
   return (
     <TaskProvider>
       <div className="App">
-        <h2>TO-DO List</h2>
+        <div className="d-flex align-items-center justify-content-beetwen mb-3">
+          <h2 className='w-100 text-left'>TODO</h2>
+          <button className="btn-theme">
+            <img src={iconMoon} alt="Icon dark mode" className="" />
+          </button>
+        </div>
         <TaskForm />
         <TaskList />
         <FilterControls />
